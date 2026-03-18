6e409d774779a87d51140c7907aecbd455a88317
sseidu96 <safiatus65@gmail.com>
2026-03-18 01:01:46 -0400

    ticket 14

--- splunk-install.sh
+++ splunk-install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # Author: Safiatu Seidu
-# Date: March 14, 2026
-# Description: Automated Splunk Enterprise Centos
-# download the splunk enterprise tar file
+# Date: March 18, 2026
+# Description: Automated Splunk Enterprise installation on Linux (CentOS)
+
+### SPLUNK INSTALLATION SCRIPT ###
+
+
+
 
 cd /opt
 
 sudo yum install wget -y
 
-sudo wget -o splunk-9.0.4.1-419ad9369127-Linux-x86_64.tgz "https://downloads.splunk.com/products/splunk/release/9.0.4.1/Linux/splunk-9.0.4.1-419ad369127-Linux-x86_64.tgz"
+sudo wget -O splunk-9.0.4.1-419ad9369127-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/9.0.4.1/linux/splunk-9.0.4.1-419ad9369127-Linux-x86_64.tgz
+
+
 
 #Extract the tar file to /opt
-sudo tar-zxvf splunk-9.0.4.1-419ad9369127-Linux-x86_64 tgz -C /opt
+sudo tar -zxvf splunk-9.0.4.1-419ad9369127-Linux-x86_64 tgz -C /opt
 
 sudo rm -rf splunk-9.0.4.1-419ad9369127-Linux-x86_64 tgz 
 
@@ -29,6 +35,4 @@ sudo ./splunk enable boot-start
 #username= admin  and password = abcd1234 ( this can be change in the script.)
 echo "please go on the browser, access splunk on port 8000 , username: admin password: abcd1234 "
 sleep 4
-
-
 
---
+++ splunk.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Author: Safiatu Seidu
+# Date: March 18, 2026
+# Description: Automated Splunk Enterprise installation on Linux (CentOS)
+
+### SPLUNK INSTALLATION SCRIPT ###
+
+# Go to /opt directory
+cd /opt
+sleep 2
+
+# Install wget if not installed
+sudo yum install -y wget
+sleep 2
+
+# Download the Splunk Enterprise tar file
+sudo wget -O splunk-9.0.4.1-419ad9369127-Linux-x86_64.tgz \
+https://download.splunk.com/products/splunk/releases/9.0.4.1/linux/splunk-9.0.4.1-419ad9369127-Linux-x86_64.tgz
+sleep 5
+
+# Extract the tar file to /opt
+sudo tar -zxvf splunk-9.0.4.1-419ad9369127-Linux-x86_64.tgz -C /opt
+sleep 5
+
+# Remove the downloaded tar file
+sudo rm -f splunk-9.0.4.1-419ad9369127-Linux-x86_64.tgz
+sleep 2
+
+# Move into Splunk bin directory
+cd /opt/splunk/bin
+sleep 2
+
+# Start Splunk Enterprise and set up admin user & password
+sudo ./splunk start --accept-license --answer-yes --no-prompt --seed-passwd "abcd1234"
+sleep 10
+
+# Enable Splunk to start at boot
+sudo ./splunk enable boot-start
+sleep 5
+
+# Instructions to access Splunk
+echo "Please open a browser and access Splunk on port 8000"
+echo "Username: admin"
+echo "Password: abcd1234"
+sleep 4
